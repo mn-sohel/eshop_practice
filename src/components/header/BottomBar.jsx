4be3bdfad425069276b6e2a92f1d23881215ecb5
mn-sohel <sohel.m0087@gmail.com>
2025-05-17 14:56:23 +0600

    header bottombar part done
    header bottombar- part-6

--- src/components/header/BottomBar.jsx
+++ src/components/header/BottomBar.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import Container from '../commonLayouts/Container'
+import { Link } from 'react-router-dom'
+import { FaAngleDown, FaBars } from 'react-icons/fa'
 
 const BottomBar = () => {
   return (
-    <div>BottomBar</div>
+    <div className='bg-[#FF624C]'>
+      <Container>
+        <div className='flex justify-between items-center py-6'>
+          <div>
+            <ul className='flex items-center gap-20 font-["Montserrat"] font-bold text-base leading-6 text-white'>
+              <li>
+                <Link className='flex items-center gap-x-4' to={"/product-details"}><FaBars />All Categories</Link>
+              </li>
+              <li>
+                <Link className='flex items-center gap-x-4' to={"/product-list"}>Products<FaAngleDown /></Link>
+              </li>
+              <li>
+                <Link to={"/blog"}>Blog</Link>
+              </li>
+              <li>
+                <Link to={"/contact"}>Contact</Link>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <ul className='flex items-center gap-20 font-["Montserrat"] font-bold text-base leading-6 text-white'>
+              <li>
+                <Link className='flex items-center gap-x-4' to={"#"}>LIMITED SALE 👋🏻</Link>
+              </li>
+              <li>
+                <Link to={"#"}>Best Seller</Link>
+              </li>
+              <li>
+                <Link to={"#"}>New Arrival</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </Container>
+    </div>
   )
 }
 
