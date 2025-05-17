3cc9c2bd8510913de7a0cb715ebd90262d47e1d0
mn-sohel <sohel.m0087@gmail.com>
2025-05-17 16:02:49 +0600

    button problem solution
    button issue part-8

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
-const Button = ({text, bg="#FF624C", fontName ="Montserrat", fontWeight="bold", size="xl", color="white", paddingY="4", paddingX="10", radius="10"}) => {
+const Button = ({text, bg="#FF624C", fontName ="Montserrat", fontWeight="bold", size="xl", color="white", paddingY="16", paddingX="40", radius="10"}) => {
   return (
-    <button className={`bg-[${bg}] font-${fontName} font-${fontWeight} text-${size} text-${color} py-${paddingY} px-${paddingX} rounded-[${radius}px]`}>{text}</button>
+    <button 
+    className={`bg-[${bg}] font-${fontName} font-${fontWeight} text-${color} py-${paddingY} px-${paddingX} rounded-[${radius}px]`}
+    style={{
+        fontSize: "20px",
+        padding: paddingY+"px" + " " + paddingX+"px" 
+    }}
+    >{text}</button>
   )
 }
 
