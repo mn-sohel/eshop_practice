5ca699befa710a1dc125d660806d1278f4430fa0
mn-sohel <sohel.m0087@gmail.com>
2025-05-18 16:22:28 +0600

    category & product dd updated
    category & product

--- src/components/header/BottomBar.jsx
+++ src/components/header/BottomBar.jsx
@@ -1,20 +1,71 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Container from '../commonLayouts/Container'
 import { Link } from 'react-router-dom'
 import { FaAngleDown, FaBars } from 'react-icons/fa'
 
 const BottomBar = () => {
+  const [isCategoryDropdownOpen, setIsCategoryDropdownOpen] = useState(false);
+  const [isProductDropdownOpen, setIsProductDropdownOpen] = useState(false);
+
+  const categoryDropdownRef = useRef(null);
+  const productDropdownRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutSite = (event) => {
+      if(categoryDropdownRef.current && !categoryDropdownRef.current.contains(event.target)){
+        setIsCategoryDropdownOpen(false)
+      }
+
+      if(productDropdownRef.current && !productDropdownRef.current.contains(event.target)){
+        setIsProductDropdownOpen(false)
+      }
+      
+    }
+    document.addEventListener('mousedown', handleClickOutSite)
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutSite)
+    }
+  },[])
+  
+  const handleCategoryDropdown = () => {
+    setIsCategoryDropdownOpen(!isCategoryDropdownOpen)
+  }
+  
+  const handleProductDropdown = () => {
+    setIsProductDropdownOpen(!isProductDropdownOpen)
+  }
+
   return (
     <div className='bg-[#FF624C]'>
       <Container>
         <div className='flex justify-between items-center py-6'>
           <div>
             <ul className='flex items-center gap-20 font-["Montserrat"] font-bold text-base leading-6 text-white'>
-              <li>
-                <Link className='flex items-center gap-x-4' to={"/product-details"}><FaBars />All Categories</Link>
+              <li className='relative' ref={categoryDropdownRef}>
+                <button className='flex items-center gap-x-4'  onClick={handleCategoryDropdown}><FaBars />All Categories</button>
+                {isCategoryDropdownOpen && (
+                  <div className='absolute z-10 mt-2 w-48 bg-white rounded shadow-lg'>
+                    <ul className='py-2 font-["Montserrat"] font-normal text-base leading-6 text-black'>
+                      <li className='px-4 py-2 hover:bg-gray-500 cursor-pointer'>Product-1</li>
+                      <li className='px-4 py-2 hover:bg-gray-500 cursor-pointer'>Product-2</li>
+                      <li className='px-4 py-2 hover:bg-gray-500 cursor-pointer'>Product-3</li>
+                      <li className='px-4 py-2 hover:bg-gray-500 cursor-pointer'>Product-4</li>
+                    </ul>
+                  </div>
+                )}
               </li>
-              <li>
-                <Link className='flex items-center gap-x-4' to={"/product-list"}>Products<FaAngleDown /></Link>
+              <li className='relative' ref={productDropdownRef}>
+                <button className='flex items-center gap-x-4' onClick={handleProductDropdown}>Products<FaAngleDown /></button>
+                {isProductDropdownOpen && (
+                  <div className='absolute z-10 mt-2 w-48 bg-white rounded shadow-lg'>
+                    <ul className='py-2 font-["Montserrat"] font-normal text-base leading-6 text-black'>
+                      <li className='px-4 py-2 hover:bg-gray-200 cursor-pointer'>Product-5</li>
+                      <li className='px-4 py-2 hover:bg-gray-200 cursor-pointer'>Product-6</li>
+                      <li className='px-4 py-2 hover:bg-gray-200 cursor-pointer'>Product-7</li>
+                      <li className='px-4 py-2 hover:bg-gray-200 cursor-pointer'>Product-8</li>
+                    </ul>
+                  </div>
+                )}
               </li>
               <li>
                 <Link to={"/blog"}>Blog</Link>
--- src/components/header/topbarComponents/TopRightBar.jsx
+++ src/components/header/topbarComponents/TopRightBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import { FaAngleDown, FaFacebookF, FaTwitter } from 'react-icons/fa';
 import { IoLogoInstagram } from 'react-icons/io';
 import { Link } from 'react-router-dom';
@@ -7,14 +7,27 @@ const TopRightBar = () => {
     const [selectedCountry, setSelectedCountry] = useState(null);
     const [isOpen, setIsOpen] = useState(false);
 
+    const countryDrowdownRef = useRef(null)
+
     const countries = [
-    { name: 'United State', value: 'US', flag: 'https://flagcdn.com/16x12/us.png' },
-    { name: 'Canada', value: 'CA', flag: 'https://flagcdn.com/16x12/ca.png' },
-    { name: 'United King', value: 'GB', flag: 'https://flagcdn.com/16x12/gb.png' },
-    { name: 'Australia', value: 'AU', flag: 'https://flagcdn.com/16x12/au.png' },
-    { name: 'Germany', value: 'DE', flag: 'https://flagcdn.com/16x12/de.png' },
-    { name: 'France', value: 'FR', flag: 'https://flagcdn.com/16x12/fr.png' },
-]
+        { name: "United State", value: "us", language: "en", flag: "https://flagcdn.com/16x12/us.png" },
+        { name: "Spain", value: "es", language: "es", flag: "https://flagcdn.com/16x12/es.png" },
+        { name: "France", value: "fr", language: "fr", flag: "https://flagcdn.com/16x12/fr.png" },
+        { name: "Germany", value: "de", language: "de", flag: "https://flagcdn.com/16x12/de.png" },
+        { name: "Italy", value: "it", language: "it", flag: "https://flagcdn.com/16x12/it.png" },
+        { name: "Portugal", value: "pt", language: "pt", flag: "https://flagcdn.com/16x12/pt.png" },
+        { name: "Netherlands", value: "nl", language: "nl", flag: "https://flagcdn.com/16x12/nl.png" },
+        { name: "Sweden", value: "se", language: "sv", flag: "https://flagcdn.com/16x12/se.png" },
+        { name: "Norway", value: "no", language: "no", flag: "https://flagcdn.com/16x12/no.png" },
+        { name: "Denmark", value: "dk", language: "da", flag: "https://flagcdn.com/16x12/dk.png" },
+        { name: "Bangladesh", value: "bd", language: "bn", flag: "https://flagcdn.com/16x12/bd.png" },
+        { name: "India", value: "in", language: "hi", flag: "https://flagcdn.com/16x12/in.png" },
+        { name: "Pakistan", value: "pk", language: "ur", flag: "https://flagcdn.com/16x12/pk.png" },
+        { name: "Nepal", value: "np", language: "ne", flag: "https://flagcdn.com/16x12/np.png" },
+        { name: "Bhutan", value: "bt", language: "dz", flag: "https://flagcdn.com/16x12/bt.png" },
+        { name: "Sri Lanka", value: "lk", language: "si", flag: "https://flagcdn.com/16x12/lk.png" },
+        { name: "Maldives", value: "mv", language: "dv", flag: "https://flagcdn.com/16x12/mv.png" },
+    ];
 
 const handleSelect = (country) => {
     setSelectedCountry(country)
@@ -25,12 +38,12 @@ const handleSelect = (country) => {
     <div className='flex justify-end items-center gap-[49px]'>
         <div>
             <select 
-        name="currency"
-        >
+            name="currency"
+            >
             <option value="USD">USD</option>
             <option value="BDT">BDT</option>
            
-        </select>
+            </select>
         </div>
         <div className='relative after:content-[""] after:absolute after:w-[1px] after:h-[32px] after:bg-[#BFBFBF] after:left-[-25px] after:top-[50%] after:-translate-y-1/2 before:content-[""] before:absolute before:w-[1px] before:h-[32px] before:bg-[#BFBFBF] before:left-[173px] before:top-[50%] before:-translate-y-1/2'>
         {/* <img src="https://flagcdn.com/16x12/us.png" /> */}
@@ -66,7 +79,7 @@ const handleSelect = (country) => {
         {/* option list */}
         {isOpen && (
             <ul className='w-[150px] absolute border border-gray-300 bg-white shadow-lg z-10'>
-                {countries.map((country,index)=>(
+                {countries.map((country)=>(
                     <li
                     key={country.value} 
                     className='flex items-center gap-2 p-2 hover:bg-gray-200 cursor-pointer'
