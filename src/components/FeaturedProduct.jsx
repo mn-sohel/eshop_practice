70cddf77f0327b50886ee34a28e9dc2c6b724e83
mn-sohel <sohel.m0087@gmail.com>
2025-05-26 16:55:55 +0600

    slider part done
    part-15

--- src/components/FeaturedProduct.jsx
+++ src/components/FeaturedProduct.jsx
@@ -1,17 +1,63 @@
 import React from 'react'
 import ProductLayout from './commonLayouts/ProductLayout'
 import Container from './commonLayouts/Container'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from 'react-slick';
+
+  function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "red" }}
+      onClick={onClick}
+    />
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "green" }}
+      onClick={onClick}
+    />
+  );
+}
+
 
 const FeaturedProduct = () => {
+
+
+  var settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    // autoplay: true,
+    autoplaySpeed: 1000,
+  };
+
   return (
     <Container>
-        <div className='flex gap-1'>
-            <ProductLayout/>
-            <ProductLayout/>
-            <ProductLayout/>
-            <ProductLayout/>
-            <ProductLayout/>
-        </div>
+        {/* <div className='flex gap-1'> */}
+          <Slider {...settings}>
+            <ProductLayout percentTag={false} category="PHONE" title="JPhone 13 High Quality Value Buy Best Cam..." rating="5" totalRating="50" price="999.00" border={true} bg="transparent" stock={true} stockAmount="120"/>
+            
+            <ProductLayout percentTag={true} category="AUDIO" title="WH-1000XM4 Wireless Headphones High Qu..." rating="5" totalRating="120" price="59.00" border={true} bg="transparent" stock={false} stockAmount="120"/>
+            
+            <ProductLayout percentTag={false} category="LAPTOP" title="S21 Laptop Ultra HD LED Screen Feature 2023 ..." rating="5" totalRating="100" price="1199.00" border={true} bg="transparent" stock={false} stockAmount="120"/>
+            
+            <ProductLayout percentTag={false} category="CAMERA" title="Mini Polaroid Camera for Girls with Flash Li..." rating="5" totalRating="70" price="79.00" border={true} bg="transparent" stock={true} stockAmount="120"/>
+            
+            <ProductLayout percentTag={false} category="TELEVISION" title="AG OLED65CXPUA 4K Smart OLED TV New ..." rating="5" totalRating="20" price="2799.00" border={true} bg="transparent" stock={false} stockAmount="120"/>
+        </Slider>
+        {/* </div> */}
     </Container>
   )
 }
--- src/components/commonLayouts/ProductLayout.jsx
+++ src/components/commonLayouts/ProductLayout.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaStar } from 'react-icons/fa'
 import { IoCartOutline } from 'react-icons/io5'
 import { CiHeart, CiShare2 } from 'react-icons/ci'
 
-const ProductLayout = () => {
+const ProductLayout = ({
+    percentTag, 
+    category, 
+    title, 
+    rating,
+    totalRating,
+    price,
+    border,
+    bg,
+    stock,
+    stockAmount
+}) => {
+
+    let [ratingValue, setRatingValue] = useState(new Array(+rating).fill(rating))
+
   return (
-    <div className='border-1 border-transparent hover:border-[#C3C3C3] duration-300 rounded-lg p-6 group'>
+    <div style={{background:bg}} className='border-1 border-transparent hover:border-[#C3C3C3] duration-300 rounded-lg p-6 group'>
         <div className='relative'>
             <img className='w-full' src="images/productImg.png" alt="product_image" />
+            {percentTag &&
             <div className='bg-[#FF624C] py-[7px] px-5 rounded-md font-["Montserrat"] font-bold text-base text-white absolute top-[-8px] right-[-9px]'>50%</div>
+            }
             <div className='flex items-center absolute bottom-[6px] left-[48%] -translate-x-[50%] gap-5 scale-0 group-hover:scale-100 duration-300'>
                 <div className='w-[50px] h-[50px] rounded-full border border-[#FF624C] flex items-center justify-center text-[25px] bg-white text-[#FF624C] hover:bg-[#FF624C] hover:text-white duration-300 cursor-pointer'>
                     <IoCartOutline />
@@ -22,17 +38,21 @@ const ProductLayout = () => {
             </div>
         </div>
         <div>
-            <p className='font-["Montserrat"] font-normal text-sm leading-5 uppercase tracking-[5px] mt-[46px] mb-4'>PHONE</p>
-            <h3 className='font-["Poppins"] font-semibold text-xl leading-[30px] group-hover:text-[#FF624C] group-hover:underline'>JPhone 13 High Quality Value Buy Best Cam...</h3>
+            <p className='font-["Montserrat"] font-normal text-sm leading-5 uppercase tracking-[5px] mt-[46px] mb-4'>{category}</p>
+            <h3 className='font-["Poppins"] font-semibold text-xl leading-[30px] group-hover:text-[#FF624C] group-hover:underline'>{title}</h3>
             <div className='flex items-center gap-2 text-[#FFD550] mt-1 mb-6'>
-                <FaStar />
-                <FaStar />
-                <FaStar />
-                <FaStar />
-                <FaStar />
-                <span className='text-[#303030] font-["Montserrat"]  font-normal text-base'>( 100 )</span>
+                {ratingValue.map((item, index) => (
+                    <FaStar />
+                ))}
+                <span className='text-[#303030] font-["Montserrat"]  font-normal text-base'>( {totalRating} )</span>
             </div>
-            <p className='font-["Poppins"] font-semibold text-2xl leading-[30px]'>$1199.00</p>
+            <p className='font-["Poppins"] font-semibold text-2xl leading-[30px]'>$ {price}</p>
+            {stock && 
+                <div className='w-full h-[30px] bg-[#ddd] rounded-[25px] relative'>
+                    <div className='w-1/2 h-[30px] bg-[#333] rounded-[25px]'></div>
+                    <p className='absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] text-white font-bold font-["Montserrat"] text-sm'>$ {stockAmount} AVAILABLE</p>
+                </div>
+            }
         </div>
     </div>
   )
